92e7d1f4941cb1fbf23665d3bb52df8d36d763fa
Athena Serene <aserenehoosang@gmail.com>
2023-03-06 15:51:55 -0800

    created EntrySerializer

---
+++ journal/serializers.py
@@ -0,0 +1,9 @@
+from .models import Entry
+from rest_framework import serializers
+
+class EntrySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        #Which model will get serialized by the class
+        model= Entry
+        #which fields should be included in the output
+        fields = ['date','title', 'body', 'timestamp']
