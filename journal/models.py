2e697f7a0a81e547e438e1f869213480310f6936
Athena Serene <aserenehoosang@gmail.com>
2023-03-06 15:49:25 -0800

    created model & migrated

--- journal/models.py
+++ journal/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+class Entry(models.Model):
+    date = models.DateField(default=timezone.now)
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    timestamp = models.DateTimeField(auto_now=True)
