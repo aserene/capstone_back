e642fbe79e382c1457b6640b4fb3e48e382828f0
Athena Serene <aserenehoosang@gmail.com>
2023-03-06 16:00:47 -0800

    urls.py done

--- capstone_back/urls.py
+++ capstone_back/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from rest_framework import routers
+from journal.views import EntryViewSet
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
--- journal/views.py
+++ journal/views.py
@@ -4,7 +4,7 @@ from rest_framework import viewsets
 from rest_framework import permissions
 from .serializers import EntrySerializer
 # Create your views here.
-class WareViewSet (viewsets.ModelViewSet):
+class EntryViewSet (viewsets.ModelViewSet):
     queryset=Entry.objects.all()
     serializer_class=EntrySerializer
     permission_classes=[permissions.AllowAny]
