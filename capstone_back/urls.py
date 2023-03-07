e3fe8395d9b13ba435babbf1dc20471f187d75d2
Athena Serene <aserenehoosang@gmail.com>
2023-03-06 16:11:22 -0800

    updated urls.py

--- capstone_back/urls.py
+++ capstone_back/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework import routers
 from journal.views import EntryViewSet
+router= routers.DefaultRouter()
+router.register(r'journal', EntryViewSet)
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
